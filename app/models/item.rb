64c881d2fc4a387703c977dd2663f79733c3dd60
ZeninD <zenindzmitry@gmail.com>
2017-05-23 17:39:21 +0300

    creating and usind callbacks in models

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,12 @@ class Item < ActiveRecord::Base
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, length: { minimum: 2 }
   validates :name, :description, presence: true
+
+  belongs_to :category
+
+  after_initialize { puts "initialized" }  # Item.new; Item.first
+  after_save       { puts "saved" }  # Item.save; Item.create; Item.update_attributes
+  after_create     { puts "created" }
+  after_update     { puts "updated" }
+  after_destroy    { puts "destroyed" }  #Item.destroy
 end
