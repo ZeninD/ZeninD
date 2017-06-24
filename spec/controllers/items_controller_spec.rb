24bf7b670af53215b1b1efc798ff7bdca340191d
ZeninD <zenindzmitry@gmail.com>
2017-06-24 14:04:51 +0300

    controllers testing, part 2

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -10,10 +10,24 @@ describe ItemsController do
       # response.should render_template("show") # expecting <"show"> but was a redirect to <http://test.host/users/sign_in>
     # end
 
-    it "renders 404 page if an item is not found" do
-      get :show, { id: 0 }
-      response.status.should == 302
-    end
+    # it "renders 404 page if an item is not found" do
+      # get :show, { id: 0 }
+      # response.status.should == 404
+    # end
+
+  end
+
+  describe "create action" do
+
+    # it "redirects to item path if validation pass" do
+      # post :create, item: { name: 'Item 1', price: '10' }
+      # response.should redirect_to(item_path(assigns(:item)))
+    # end
+
+    # it "renders new page again if validations fail" do
+      # post :create, item: { name: 'Item 1', price: 0 }
+      # response.should render_template("new") # expecting <"new"> but was a redirect to <http://test.host/users/sign_in>
+    # end
 
   end
 
