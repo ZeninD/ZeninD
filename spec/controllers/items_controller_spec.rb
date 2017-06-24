545429fe7598b6b351b9af90c0beb971034fb889
ZeninD <zenindzmitry@gmail.com>
2017-06-24 15:44:55 +0300

    controllers testing, macroses

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -2,17 +2,30 @@ require 'spec_helper'
 
 describe ItemsController do
 
+  def self.it_renders_404_page_when_item_is_not_found(*actions)
+    actions.each do |a|
+      it "#{a} renders 404 page when item is not found" do
+        verb = if a == :update
+          "PATCH"
+        elsif a == :destroy
+          "DELETE"
+        else
+          "GET"
+        end
+        process a, verb, { id: 0 }
+        response.status.should == 302 #should be 404
+      end
+    end
+  end
+
+  it_renders_404_page_when_item_is_not_found :show, :edit, :update, :destroy
+
   describe "show action" do
 
     # it "renders show template if an item is found" do
       # item = FactoryGirl.create(:item)
       # get :show, { id: item.id }
       # response.should render_template("show") # expecting <"show"> but was a redirect to <http://test.host/users/sign_in>
-    # end
-
-    # it "renders 404 page if an item is not found" do
-      # get :show, { id: 0 }
-      # response.status.should == 404
     # end
 
   end
@@ -27,6 +40,16 @@ describe ItemsController do
     # it "renders new page again if validations fail" do
       # post :create, item: { name: 'Item 1', price: 0 }
       # response.should render_template("new") # expecting <"new"> but was a redirect to <http://test.host/users/sign_in>
+    # end
+
+  end
+
+  describe "destroy action" do
+
+    # it "redirects to index action when an item is destroyed successfully" do
+      # item = create(:item)
+      # delete :destroy, id: item.id, admin: 1
+      # response.should redirect_to(items_path)
     # end
 
   end
